2fec85ecf06f6c7e9a61efe5b3e819ae735bbd1f
Joshua Williams <102987390+jwlv@users.noreply.github.com>
2025-02-18 10:39:29 -0800

    CASMHMS-5678: Attempt fix resty issue

--- internal/smdclient/smdclient.go
+++ internal/smdclient/smdclient.go
@@ -110,8 +110,7 @@ func Init(restRetry int, restTimeout int, hsmURL string, svcName string) error {
 	rClient = resty.New().
 		SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true}).
 		SetTimeout(time.Duration(time.Duration(restTimeout) * time.Second)).
-		SetRetryCount(restRetry). // This uses a default backoff algorithm
-		SetRESTMode()             // This enables automatic unmarshalling to JSON and no redirects
+		SetRetryCount(restRetry) // This uses a default backoff algorithm
 
 	hsm = hsmURL
 
